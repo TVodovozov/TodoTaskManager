94878e5d8142f25e7d521cdb4f2dc3c0351ab216
Timur <vodovozzz@inbox.ru>
2023-02-08 00:13:25 +0300

    lesson_1 Fix some bugs

--- userapp/models.py
+++ userapp/models.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 
 class User(AbstractUser):
-    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=50, blank=True, null=True, unique=True)
+    first_name = models.CharField(max_length=150, blank=True, null=True, unique=True)
+    last_name = models.CharField(max_length=150, blank=True, null=True, unique=True)
+    email = models.EmailField(_("email address"), unique=True)
--- userapp/serializers.py
+++ userapp/serializers.py
@@ -6,5 +6,4 @@ from .models import User
 class UserModelSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = "__all__"
-        # fields = ('username', 'first_name', 'last_name', 'email')
+        fields = ("username", "first_name", "last_name", "email")
