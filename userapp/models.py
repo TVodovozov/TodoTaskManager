6d76468aaf31ccb3a744a95ef568f42ba8fbf73a
Timur <vodovozzz@inbox.ru>
2023-02-08 23:50:27 +0300

    Lesson 1 (#1)
    * lesson_1 Lesson 1 ready.

--- userapp/models.py
+++ userapp/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = models.CharField(max_length=50, blank=True, null=True, unique=True)
+    first_name = models.CharField(max_length=150, blank=True, null=True, unique=True)
+    last_name = models.CharField(max_length=150, blank=True, null=True, unique=True)
+    email = models.EmailField(_("email address"), unique=True)
