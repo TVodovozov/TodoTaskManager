4cafb9797c311de75653b7bef05715310f6108ca
Timur <vodovozzz@inbox.ru>
2023-02-19 22:01:50 +0300

    lesson_4 add models Project and ToDo (#4)

--- userapp/views.py
+++ userapp/views.py
@@ -1,9 +1,9 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import mixins, viewsets
 
 from .models import User
 from .serializers import UserSerializer
 
 
-class UserViewSet(ModelViewSet):
+class UserViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, viewsets.GenericViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
