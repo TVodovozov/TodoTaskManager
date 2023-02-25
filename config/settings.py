d7aa0b7802ff183b040a9e7b95b5e8b0bbab2c7e
Timur <vodovozzz@inbox.ru>
2023-02-25 22:33:36 +0300

    lesson_5 add react-router-dom (#5)

--- config/settings.py
+++ config/settings.py
@@ -145,8 +145,10 @@ REST_FRAMEWORK = {
     "PAGE_SIZE": 100,
 }
 
-# CORS
+## CORS
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
+    "http://127.0.0.1:37745",
     "http://localhost:3000",
+    "http://localhost:37745",
 ]
