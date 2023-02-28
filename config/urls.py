c79c6530e7e62605970698dd15c4e6650e6ba2b0
Timur <vodovozzz@inbox.ru>
2023-02-28 22:33:13 +0300

    lesson_6 add  authorisation

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from todoapp.views import ProjectViewSet, ToDoViewSet
 from userapp.views import UserViewSet
@@ -29,4 +30,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("api/", include(router.urls)),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
